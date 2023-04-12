0ce228620e8138dd0b51db97034296f637dc5248
Singh Shekhawat <ajit.singh-shekhawat@capgemini.com>
2023-04-12 12:25:57 +0200

    New version

--- dist/index.js
+++ dist/index.js
@@ -11899,7 +11899,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 const sodium = __nccwpck_require__(7637)
-
+const fs = __nccwpck_require__(7147);
 class GithubLocation {
   constructor(location_input) {
     this.type = "repository"
